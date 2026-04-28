e732bbcded2c482705169f344b42d4190bb61ea4
Mxtvey <matveysergeev1234@gmail.com>
2026-04-28 16:31:31 +0300

    выделил проверку серверов в отдельный класс

--- LoadBalancer/Balance/WeightedRoundRobinStrategy.cs
+++ LoadBalancer/Balance/WeightedRoundRobinStrategy.cs
@@ -9,30 +9,7 @@ public class WeightedRoundRobinStrategy : IBalanceStrategy
 
     public ServerCondition GetFreeServer(List<ServerCondition> servers)
     {
-        if (servers is null)
-            throw new InvalidServersCollectionException("Servers list is null.");
-
-        if (servers.Count == 0)
-            throw new InvalidServersCollectionException("Servers list is empty.");
-
-        if (servers.Any(s => s is null))
-            throw new InvalidServersCollectionException("Servers list contains null elements.");
-
-        var aliveServers = servers.Where(s => s.IsAlive).ToList();
-        if (aliveServers.Count == 0)
-            throw new NoAliveServersException("There are no alive servers.");
-
-        if (aliveServers.Any(s => s.ServerInfo is null))
-            throw new InvalidServersCollectionException("Server info is null.");
-
-        if (aliveServers.Any(s => string.IsNullOrWhiteSpace(s.ServerInfo.Host)))
-            throw new InvalidServersCollectionException("Server host is empty.");
-
-        if (aliveServers.Any(s => s.ServerInfo.Port <= 0))
-            throw new InvalidServersCollectionException("Server port is invalid.");
-
-        if (aliveServers.Any(s => s.Weight <= 0))
-            throw new InvalidServersCollectionException("Server weight must be positive.");
+        var aliveServers = BalanceValidation.GetValidatedAliveServers(servers);
 
         var totalWeight = aliveServers.Sum(s => s.Weight);
 
